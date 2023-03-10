8d87cfc9d95ae7b9756c335d228bef3b463a330b
Gurman Singh <gurmandhanoa01@gmail.com>
2023-03-10 22:40:37 +0530

    "/" get request added

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,17 @@ const doctorRoutes = require("./routes/doctorRoute");
 const userRoutes = require("./routes/userRoute");
 const appointmentRoute = require("./routes/appointmentRoute");
 
-app.use("/api/v1",doctorRoutes);
-app.use("/api/v1",userRoutes);
-app.use("/api/v1",appointmentRoute);
+app.use("/api/v1", doctorRoutes);
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", appointmentRoute);
 
 // using middleware
 app.use(ErrorHandler);
+
+app.get("/", (req, res) => {
+  res.send(
+    `<h1 style="font-family:Garamond; width:100%; text-align:center; padding-top: 45vh;">Server is working fine, <a href="${process.env.FRONTEND_URL}" style="font-style: italic;">click here</a> to visit client side.</h1>`
+  );
+});
 
 module.exports = app;
